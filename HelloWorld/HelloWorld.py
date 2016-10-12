1dfa8a4158e60c3a33a605c3ddeca5081b06d9be
João Paulo Souza Reis <joaoreis13@gmail.com>
2016-10-12 18:04:43 -0300

    Added the config

--- HelloWorld/HelloWorld.py
+++ HelloWorld/HelloWorld.py
@@ -1,10 +1,11 @@
 from flask import Flask
 
 application = Flask(__name__)
+application.config.from_object('config')
 
 @application.route('/')
 def hello_world():
-	return "Helo World! fase 2"
+	return "Helo World! fase 2"+application.config['CONFIG_VAR']
 
 if __name__ == '__main__':
 	application.run()
