7baeea460386841f3cdcceaa7ba72c4f15254e9c
João Paulo Souza Reis <joaoreis13@gmail.com>
2016-10-15 01:40:40 -0300

    Creating custom returns

--- HelloWorld/HelloWorld.py
+++ HelloWorld/HelloWorld.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask,jsonify
 
 ftHelloWorld = Flask(__name__)
 ftHelloWorld.config.from_object('config')
 
 @ftHelloWorld.route('/')
 def hello_world():
-	return "Helo World! fase 2"+ftHelloWorld.config['CONFIG_VAR'],202
+    ret = jsonify( {"Helo World! fase 2 ":ftHelloWorld.config['CONFIG_VAR']} )
+    ret.status_code = 202
+
+    return ret
 
 if __name__ == '__main__':
 	ftHelloWorld.run()
