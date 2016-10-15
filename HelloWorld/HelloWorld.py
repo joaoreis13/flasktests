57986d2bc6f77d950bb1e58442f9fe2dcc9b664c
João Paulo Souza Reis <joaoreis13@gmail.com>
2016-10-15 01:05:25 -0300

    testing custom error return

--- HelloWorld/HelloWorld.py
+++ HelloWorld/HelloWorld.py
@@ -5,7 +5,7 @@ ftHelloWorld.config.from_object('config')
 
 @ftHelloWorld.route('/')
 def hello_world():
-	return "Helo World! fase 2"+ftHelloWorld.config['CONFIG_VAR']
+	return "Helo World! fase 2"+ftHelloWorld.config['CONFIG_VAR'],202
 
 if __name__ == '__main__':
 	ftHelloWorld.run()
